2f170a07e35db882d04fd8bdda8d4bcee07fa9ae
Omar Aied <omaraied007@gmail.com>
2026-02-03 13:19:34 +0000

    Created function to update the parameters of the weights and biases

--- main.py
+++ main.py
@@ -61,3 +61,11 @@ def back_prop(Z1, A1, Z2, A2, W2, X, Y):
     dW1 = (1 / Y.size) * dZ1.dot(X.T)
     db1 = (1 / Y.size) * np.sum(dZ1,2)
     return dW1, db1, dW2, db2
+
+# Update parameters
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, learning_rate):
+    W1 -= learning_rate * dW1
+    b1 -= learning_rate * db1
+    W2 -= learning_rate * dW2
+    b2 -= learning_rate * db2
+    return W1, b1, W2, b2
